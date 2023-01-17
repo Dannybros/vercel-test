d4fffb984f754d77cbf8949494dc92ebf339978f
Dannybros <akbaromega89@gmail.com>
2023-01-17 15:05:11 +0700

    testing vercel 1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const port = process.env.PORT || 9000;
 
 app.get('/', (req, res)=>{
-    res.send("Hello World");
+    res.send("Hello Danny");
 })
 
 app.listen(port, ()=>console.log(`Server starting in port ${port}`));
