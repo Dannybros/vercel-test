ed09c6b596d97ab009eb89cf45e328a168011fa8
Dannybros <akbaromega89@gmail.com>
2023-01-18 11:12:18 +0700

    testing vercel mongodb and env

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 9000;
 
-// mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", false);
 
-// mongoose.connect(process.env.MONGODB_URI,{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true, 
-// })
-// mongoose.connection.on("connected", ()=>{
-//     console.log("Mongoose is connected");
-// })
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+})
+mongoose.connection.on("connected", ()=>{
+    console.log("Mongoose is connected");
+})
 
 app.use(express.json());
 app.use(cors());
