c0b8729bf6dacfe3b4a0c2b65be770d04578f89a
Dannybros <akbaromega89@gmail.com>
2023-01-18 10:59:42 +0700

    testing mongodb on vercel

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import donation from './routes/Donation.js'
-import news from './routes/News.js'
 import cases from './routes/Cases.js'
-import signIn from './routes/SignIn.js'
-import stripe from './routes/Stripe.js'
-import paypal from './routes/Paypal.js'
+// import donation from './routes/Donation.js'
+// import news from './routes/News.js'
+// import signIn from './routes/SignIn.js'
+// import stripe from './routes/Stripe.js'
+// import paypal from './routes/Paypal.js'
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+mongoose.set("strictQuery", false);
 
 mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser: true,
@@ -24,19 +28,19 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res)=>{
-    res.send("Hello BOy");
+    res.send("testing mongodb");
 })
 
-app.use('/news-image', express.static('news-image'))
-app.use('/project-image', express.static('project-image'))
+// app.use('/news-image', express.static('news-image'))
+// app.use('/project-image', express.static('project-image'))
 
 // endpoints
-app.use('/donation', donation)
-app.use('/stripe', stripe)
-app.use('/news', news)
 app.use('/cases', cases)
-app.use('/signIn', signIn)
-app.use('/paypal', paypal)
+// app.use('/donation', donation)
+// app.use('/stripe', stripe)
+// app.use('/news', news)
+// app.use('/signIn', signIn)
+// app.use('/paypal', paypal)
 
 
 app.listen(port, ()=>console.log(`Server starting in port ${port}`));
--- routes/Cases.js
+++ routes/Cases.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import CaseCollection from '../models/CaseModel.js'
-import projImg from '../middlewares/ProjImg.js'
-import auth from '../middlewares/auth.js'
-import { uploadFile } from '../firebase/index.js';
-import { storage } from '../firebase/firebase.js';
-import { ref, getDownloadURL, deleteObject } from "firebase/storage";
+import NewsCollection from '../models/NewsModel.js'
+// import projImg from '../middlewares/ProjImg.js'
+// import auth from '../middlewares/auth.js'
+// import { uploadFile } from '../firebase/index.js';
+// import { storage } from '../firebase/firebase.js';
+// import { ref, getDownloadURL, deleteObject } from "firebase/storage";
 
 const router = express.Router();
 
@@ -18,13 +19,8 @@ router.get('/', (req, res)=>{
     })
 })
 
-router.get('/topThree', (req, res)=>{
-     let query = CaseCollection
-            .find()
-            .sort({reach: -1})
-            .limit(3);
-
-    query.exec(function (err, data){
+router.get('/news', (req, res)=>{
+    NewsCollection.find({}, (err, data)=>{
         if(err){
             res.status(500).send(err);
         }else{
@@ -32,109 +28,5 @@ router.get('/topThree', (req, res)=>{
         }
     })
 })
-
-router.get('/get/:id', (req, res)=>{
-    const id = req.params.id
-
-    CaseCollection.find({_id: id}, (err, data)=>{
-        if(err){
-            res.status(500).send(err);
-        }else{
-            res.status(201).send(data);
-        }
-    })
-})
-
-router.post('/delete/:id', auth, async(req, res)=>{
-    const id = req.params.id;
-    const img = req.body;
-
-    await img.map((file)=>{
-        try {
-            deleteObject(ref(storage, `${file}`))
-        } catch (error) {
-            console.log(error);
-            res.status(400).send(error.message);
-        }
-    })
-
-    CaseCollection.findByIdAndDelete({_id: id},({new:true}), (err, data)=>{
-        if(err){
-            res.status(500).send(err);
-        }else{
-            res.status(201).json({message:"successfully deleted", data:data});
-        }
-    })
-})
-
-router.post('/update', (req, res)=>{
-    const {id, amount} = req.body
-    
-    const update ={$inc: {reach:amount}}
-
-    CaseCollection.findByIdAndUpdate(id, update, ({new:true}), (err, data)=>{
-        if(err){
-            res.status(500).send(err)
-        }else{
-            res.status(201).send(data)
-        }
-    })
-})
-
-
-router.post('/', auth, projImg.array('img'), async(req, res)=>{ 
-
-    if(req.body.titleEn==="" || req.body.titleZh==="" || req.body.titleKo==="" || req.body.goal ==="" || req.body.contentEn==="" ||
-    req.body.contentZh==="" ||req.body.contentKo===""){
-
-        res.status(500).json({message:"fill in all the input"})
-        
-    }else{
-        
-        const files = req.files;
-
-        const imgList =await Promise.all(files.map(async(file)=>{
-            try {
-                await uploadFile(`${file.destination}${file.filename}`, `images/${file.filename}`);
-        
-                return getDownloadURL(ref(storage, `images/${file.filename}`))
-                .then((url)=>{
-                    const string = url;
-                    return string;
-                });
-                
-            } catch (error) {
-                console.log (error)
-                res.status(400).send(error.message);
-            }
-        }))
-        
-        var DBobj = {
-            title:{
-                en:req.body.titleEn,
-                zh:req.body.titleZh,
-                ko:req.body.titleKo
-            },
-            goal:req.body.goal,
-            reach:0,
-            content:{
-                en:req.body.contentEn,
-                zh:req.body.contentZh,
-                ko:req.body.contentKo
-            },
-            img:imgList
-        }
-    
-        await CaseCollection.create(DBobj, (err, data)=>{
-            if(err){
-                res.status(501).send(err);
-            }
-            else{
-                res.status(200).send("Success");
-            }
-        })
-    }
-})
-
 
 export default router;
