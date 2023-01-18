ed5d25b19c7ee3055c91dadf969c76c7a2d51eb1
Dannybros <akbaromega89@gmail.com>
2023-01-18 11:08:49 +0700

    tesing version 5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import cases from './routes/Cases.js'
+// import cases from './routes/Cases.js'
 // import donation from './routes/Donation.js'
 // import news from './routes/News.js'
 // import signIn from './routes/SignIn.js'
@@ -14,33 +14,23 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 9000;
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true, 
-})
-mongoose.connection.on("connected", ()=>{
-    console.log("Mongoose is connected");
-})
+// mongoose.connect(process.env.MONGODB_URI,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true, 
+// })
+// mongoose.connection.on("connected", ()=>{
+//     console.log("Mongoose is connected");
+// })
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res)=>{
-    res.send("testing mongodb");
+    res.send("testing simply");
 })
-
-// app.use('/news-image', express.static('news-image'))
-// app.use('/project-image', express.static('project-image'))
-
-// endpoints
-app.use('/cases', cases)
-// app.use('/donation', donation)
-// app.use('/stripe', stripe)
-// app.use('/news', news)
-// app.use('/signIn', signIn)
-// app.use('/paypal', paypal)
+// app.use('/cases', cases)
 
 
 app.listen(port, ()=>console.log(`Server starting in port ${port}`));
