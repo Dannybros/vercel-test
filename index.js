f92f5881a3817fb5bb4b314eaa337812bf8c7ce0
Dannybros <akbaromega89@gmail.com>
2023-01-18 11:14:34 +0700

    adding case route test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-// import cases from './routes/Cases.js'
+import cases from './routes/Cases.js'
 // import donation from './routes/Donation.js'
 // import news from './routes/News.js'
 // import signIn from './routes/SignIn.js'
@@ -30,7 +30,7 @@ app.use(cors());
 app.get('/', (req, res)=>{
     res.send("testing simply");
 })
-// app.use('/cases', cases)
+app.use('/cases', cases)
 
 
 app.listen(port, ()=>console.log(`Server starting in port ${port}`));
